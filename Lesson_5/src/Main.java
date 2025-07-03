d5265b1c7e7074b5478e855a731757532fafc3d1
Alexei <axelmate6@gmail.com>
2025-07-03 23:09:25 +0300

    lesson 5

--- Lesson_5/src/Main.java
+++ Lesson_5/src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
+        String[][] array = {{"3", "5", "1", "2"},
+                {"2", "5", "8", "10"},
+                {"7", "3", "3", "2"},
+                {"-3", "5", "5", "-1"}};
+        System.out.println(arraySum(array));
+        try {
+            array[10][10] = "10";
+            System.out.println(array[10][10]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
         }
+
+    }
+
+    public static int arraySum(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        int sum = 0;
+        if (array.length != 4) throw new MyArraySizeException();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) throw new MyArraySizeException();
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        return sum;
     }
 }
---
+++ Lesson_5/src/MyArrayDataException.java
@@ -0,0 +1,5 @@
+public class MyArrayDataException extends Exception {
+    public MyArrayDataException(int i, int j) {
+        super("Неверные данные в ячейке [" + i + "][" + j + "]");
+    }
+}
